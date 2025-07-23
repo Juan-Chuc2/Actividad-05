1fb0b95727b3a728632496a39b4b1cb3ab7f0d57
Juan Diego <chucjuan508@gmail.com>
2025-07-22 16:40:39 -0800

    commit 1 caso 1 terminado

--- Actividad 05.py
+++ Actividad 05.py
@@ -1,14 +1,36 @@
+ventas = []
 while True:
-    lista = []
     print("\n --Menu de Analisis de ventas--")
-    print("1. Ingrese la lista de ventas")
-    print("2. Mostrar todas las ventas ingresadas")
-    print("3. venta mas alta y la mas baja")
-    print("4. promedio de las ventas")
-    print("5. Cuantos dias se superaron Q1000")
-    print("6. Clasificaioon de ventas")
-    print("7. Salir")
-    option=input("Elija una opcioon")
+    print("1. Ingrese la lista de ventas ")
+    print("2. Mostrar todas las ventas ingresadas ")
+    print("3. venta mas alta y la mas baja ")
+    print("4. promedio de las ventas ")
+    print("5. Cuantos dias se superaron Q1000 ")
+    print("6. Clasificaioon de ventas ")
+    print("7. Salir ")
+    option=input("Elija una opcion ")
     match option:
-        case 1:
-            venta = int(input())
+        case "1":
+            print("Ingreso de ventas")
+            day_admitted= int(input("ingrese cuantos dias quiere ingresar"))
+            for i in range (day_admitted):
+                while True:
+                    venta_= input("Ingrese la cantidad de las ventas")
+                    if venta_.isdigit():
+                        venta= int(venta_)
+                        ventas.append(venta)
+                        break
+                    else:
+                        print("Error datos invalidos ")
+        case "2":
+            print("Mostrando ventas ingresadas...")
+        case "3":
+            print("Calculando ventas...")
+        case "4":
+            print("")
+        case "5":
+            print("")
+        case "6":
+            print("hola ")
+        case "7":
+            print("Estamos slaiendo...")
